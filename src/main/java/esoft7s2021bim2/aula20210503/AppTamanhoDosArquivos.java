dadd1bdd23768b739078580bba564c33a5746b9e
arthur.zavadski <aczavads@gmail.com>
2021-05-07 20:30:10 -0300

    Excelente exemplo de listagem de arquivos em modo recursivo com uso de streams, filter, collect e flatMap. Valeu Keven pela sugestão de colocar os diretórios em primeiro lugar! :D

--- src/main/java/esoft7s2021bim2/aula20210503/AppTamanhoDosArquivos.java
+++ src/main/java/esoft7s2021bim2/aula20210503/AppTamanhoDosArquivos.java
@@ -2,6 +2,11 @@ package esoft7s2021bim2.aula20210503;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AppTamanhoDosArquivos {
 
@@ -16,7 +21,34 @@ public class AppTamanhoDosArquivos {
 	}
 
 	private static void listarArquivosNoConsole(String nomeDoDiretório) throws IOException {
+		List<String> nomesParaListar = new ArrayList<>();
+		listarArquivosNoConsole(nomeDoDiretório, nomesParaListar);
+		System.out.println("Listando: " + nomeDoDiretório);
+		for (int i = nomesParaListar.size()-1; i >= 0; i--) {
+			System.out.println(nomesParaListar.get(i));
+		}
 		
+	}
+	private static void listarArquivosNoConsole(String nomeDoDiretório, List<String> nomesParaListar) throws IOException {
+		File diretório = new File(nomeDoDiretório);
+		File[] auxDiretórios = Arrays.stream(diretório.listFiles())
+				.filter( f -> f.isDirectory())
+				.collect(Collectors.toList())
+				.toArray(File[]::new);
+		File[] auxArquivos = Arrays.stream(diretório.listFiles())
+				.filter( f -> !f.isDirectory())
+				.collect(Collectors.toList())
+				.toArray(File[]::new);
+		File[] arquivosDoDiretório = Stream.of(auxDiretórios, auxArquivos)
+				.flatMap(Stream::of)
+				.toArray(File[]::new);
+		for (File file : arquivosDoDiretório) {
+			if (file.isDirectory()) {
+				listarArquivosNoConsole(file.getCanonicalPath(), nomesParaListar);
+			} 
+			//System.out.println(file.getCanonicalPath());				
+			nomesParaListar.add(file.getCanonicalPath());
+		}		
 	}
 	private static long calcularTamanhoTotalArquivos(String nomeDoDiretório) throws IOException {
 		long total = 0;
